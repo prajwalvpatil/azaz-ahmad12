6e3765404450d5642c66f8a52f8976e990af769d
AzazAhmadusalman <124564526+AzazAhmadusalman@users.noreply.github.com>
2023-02-23 23:08:32 +0530

    Add files via upload

---
+++ aza.py
@@ -0,0 +1,16 @@
+a=1
+b=1.1
+c=1+5j
+print("type of ",type(a))
+print("type of ",type(b))
+print("type of ",type(c))
+i=[1,2,3,4,5]
+print("type of ",type(i))
+student_id=(11,112,113,114,115)
+print("type of ",type(student_id))
+name={
+    "name":"azaz","usn":22
+}
+print("type of",type(name))
+is_name=True
+print("type of",type(is_name))
